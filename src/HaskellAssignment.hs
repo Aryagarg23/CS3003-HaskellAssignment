ed5b1ab693fb47810f5f3f9c86ad817ba7e457d5
Arya Garg <garg.arya@gmail.com>
2024-12-13 20:21:34 -0500

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -9,7 +9,16 @@ instance Show Found where
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
 
+--findFirst _ {} = NoMatch
+--findFirst f (x:xs) =
+findFirst f list =doFindFirst f list 0
+
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
+
+palindrome s = s == (rvrs s)
+
+rvrs [] = []
+rvrs (x:xs) = (rvrs xs) ++ [x] 
